06d4e1a0ddc5b968469e74c26beeec88edb46733
Laurent Thomas <thomas_laurent3@ens.univ-artois.fr>
2018-10-13 17:16:38 +0200

    gradient descent

---
+++ ap/GradientDescent.py
@@ -0,0 +1,28 @@
+
+X = [1,2,4,3,5]
+Y = [1,3,3,2,5]
+
+ALPHA = 0.01
+
+def gd(max_iterations=20, b0=0, b1=0):
+
+    iteration = 0
+    while(True):
+        for i in range(len(X)):
+
+            if iteration == max_iterations:
+                return
+
+            x = X[i]
+            y = Y[i]
+            p_i = b0 + b1 * x
+            error = p_i - y
+            b0 = b0 - error * x * ALPHA
+            b1 = b1 - error * ALPHA
+
+            iteration += 1
+            print("Iteration: %3d | erreur=%5d b0=%20f , b1=%20f" % \
+            (iteration, error, b0, b1))
+    
+
+gd(max_iterations=30)
