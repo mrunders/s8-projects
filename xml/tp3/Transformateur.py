8cc491ee96074fe0c6007bcaf37f40b4d9d946d7
Laurent Thomas <thomas_laurent3@ens.univ-artois.fr>
2018-10-16 08:17:15 +0200

    trans

--- xml/tp3/Transformateur.py
+++ xml/tp3/Transformateur.py
@@ -1,100 +1,60 @@
 
 import xml.sax
 
-XML_SOURCE_FILE = "./low_dblp.xml"
+XML_SOURCE_FILE = "./dblp.xml"
 RESULT_SET_FILE = "./resultSet.txt"
-IGNORE_ATTRIBUTS = True
 ELEMENT_DELIMITER = "|"
 KEY_VALUE_DELIMITER = ":"
 
 class ItemSet():
 
-	def __init__(self, name):
+	def __init__(self):
 		self.__data = []
-		self.__header = []
-		self.__selected = False
-		self.__string_is_builded = False
+
+	def append(self, key, value, index=-1):
+		self.__data[index] += key + KEY_VALUE_DELIMITER + value + ELEMENT_DELIMITER
 	
-	def isSelected(self):
-		return self.__selected
-
-	def select(self, state=True):
-		self.__selected = state
-
-	def append(self, key, data, index=-1):
-		self.__data[index].append(key + KEY_VALUE_DELIMITER + data)
-
-	def newLine(self, data):
-		self.__data.append(data)
-
-	def appendHeader(self, header):
-		self.__header.append(header)
-
-	def getData(self):
-
-		if ( not self.__string_is_builded):
-			self.__data = "\n".join([ELEMENT_DELIMITER.join(item) for item in self.__data])
-
-		return self.__data
-
-	def getHeader(self):
-		return self.__header
+	def newLine(self):
+		self.__data.append("")
 
 
 class TransformateurXML(xml.sax.ContentHandler):
 
-	ITEMSET_NAME = "article|inproceedings|proceedings|book|incollection|phdthesis|mastersthesis|www|person|data".split("|")
+	ITEMSET_NAME = ["article","inproceedings"]
 
-	IGNORE_ATTRIBUTS_FALSE = lambda v, attrs : ["-{}{}{}".format(attr, KEY_VALUE_DELIMITER, attrs[attr]) for attr in attrs.keys(), ELEMENT_DELIMITER]
-	IGNORE_ATTRIBUTS_TRUE  = lambda v, x : []
-
-	def __init__(self):
+	def __init__(self, pattern=""):
 
 		self.__itemSet = {}
 		self.__balise_name = None
 		self.__current_balise = None
 		self.__parser = xml.sax.make_parser()
-
-		self.__initLine = self.IGNORE_ATTRIBUTS_TRUE if IGNORE_ATTRIBUTS else self.IGNORE_ATTRIBUTS_FALSE
+		self.__pattern = "author" + KEY_VALUE_DELIMITER + pattern
 
 		for item in self.ITEMSET_NAME:
-			self.__itemSet[item] = ItemSet(item)
+			self.__itemSet[item] = ItemSet()
 	
 	def startElement(self, name, attrs):
 
 		if name in self.ITEMSET_NAME:
-			self.__itemSet[name].select()
-			self.__itemSet[name].newLine( self.__initLine(attrs) )
+			self.__itemSet[name].newLine()
 			self.__balise_name = name
 
 		self.__current_balise = name
 		
 	def endElement(self, name):
-
-		if not self.__balise_name is None:
-			self.__itemSet[self.__balise_name].select(state=False)
+		pass
 	
 	def characters(self, data):
 
-		if (not data == "\n"):
+		if (not data[0] == '\\' and not self.__balise_name is None):
 			self.__itemSet[self.__balise_name].append(self.__current_balise.encode("utf-8"), data.encode("utf-8"))
 			
 	def parse(self, file_dir=XML_SOURCE_FILE):
-
+		
+		print("starting parsing")
 		self.__parser.setContentHandler(self)
 		self.__parser.parse(file_dir)
 		return self.__itemSet
-		
-	def save(self, dir_file=RESULT_SET_FILE):
-		## only for visual.
-
-		with open( dir_file, "wt" ) as file:
-			for k,v in self.__itemSet.items():
-				file.write(k)
-				file.write("\n\n")
-				file.write( v.getData())
-				file.write("\n------------------------------------------------------\n")
 	
-t = TransformateurXML()
+t = TransformateurXML(pattern="Carmen Heine")
 a = t.parse()
-t.save()
