878c23a463c0d4299e453ef9e04eb4c3b1c1e76e
mrunders <mrunder.s@gmail.com>
2018-12-11 14:34:06 +0100

    test case

--- xml/tp3/sci.sh
+++ xml/tp3/sci.sh
@@ -3,11 +3,11 @@
 let varc="$1"
 shift
 
-if test $varc -eq 2 
+if test $varc -eq 2 && test ! -f ensSet.gob && test ! -f etuSet.txt
 then
 echo "Preparing stuff"
 java -jar -DentityExpansionLimit=0 saxGuy.jar -out etuSet.txt dblp.xml
-./dblp-prof-linux2 -out ensSet.txt dblp.xml
+./dblp-prof-linux2 -out ensSet.gob dblp.xml
 fi
 
 while test $# -ne 0
@@ -24,8 +24,13 @@ diff etu.txt ens.txt
 elif test $varc -eq 2
 then
 java -jar -DentityExpansionLimit=0 saxGuy.jar -name "$1" -in etuSet.txt | sort > etu.txt &&
-./dblp-prof-linux2 -name "$1" -in ensSet.txt | sort > ens.txt &&
+./dblp-prof-linux2 -name "$1" -in ensSet.gob | sort > ens.txt &&
 diff etu.txt ens.txt
+
+elif test $varc -eq 3
+then
+java -jar -DentityExpansionLimit=0 saxGuy.jar -name "$1" dblp.xml -extract "$1.gob" &&
+echo "saxGuy has written files called $1.gob"
 fi
 
 
