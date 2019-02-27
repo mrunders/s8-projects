0b13dfbbbf72439a132ca5f14476c48e0c9c8341
mrunders <mrunder.s@gmail.com>
2019-02-27 11:33:46 +0100

    Add files via upload

--- homework/Sat_graph/sat_graph.sh
+++ homework/Sat_graph/sat_graph.sh
@@ -7,5 +7,7 @@
 ## chmod u+x
 ## exemple: ./s.sh MiniSat_v1.14_linux tmp.json 3
 
+test $# -eq 0 && echo "./sat_pgrah.sh <Solver (Minisat v1.14)> <fichier json> <nb colors>" && exit 127
+
 python __main__.py $2 $3 sat.in && ./$1 sat.in sat.out
 test $? -eq 10 && echo "======== Model ========" && python model_printer.py sat.out sat.in
